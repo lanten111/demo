f922b7d47baa3e5886255f1ba3e6beddc699a545
makhado <makhadolucky@gmail.com>
2021-02-16 22:51:30 +0200

    adding servies

--- src/main/java/com/example/demo/service/Service.java
+++ src/main/java/com/example/demo/service/Service.java
@@ -4,4 +4,27 @@ created by mumakhado on 2021/02/16
 */
 
 public class Service {
+
+    public double getCelcius(Double kelvin){
+
+        double celsius = kelvin - 273.15;
+
+        return celsius;
+
+        int start = (int) (System.currentTimeMillis() * 1000);
+
+    }
+
+    public double getKelvin(Double celsius){
+
+        double kelvin = celsius + 273.15;
+
+        return  kelvin;
+    }
+
+    public double getkilometers(double miles){
+
+    }
+
+
 }
