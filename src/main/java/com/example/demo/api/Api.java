246e45e6779cd660708aba27026385f4d5cc5e59
makhado <makhadolucky@gmail.com>
2021-02-16 23:08:49 +0200

    adding api

--- src/main/java/com/example/demo/api/Api.java
+++ src/main/java/com/example/demo/api/Api.java
@@ -3,5 +3,21 @@ package com.example.demo.api;
 created by mumakhado on 2021/02/16
 */
 
+import com.example.demo.service.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.Path;
+
+@RestController
+@Path("/conversions")
 public class Api {
+
+    Service service = new Service();
+
+    @GetMapping("/ktoc")
+    public Double getCelsius(@RequestParam Double kelvin){
+        return  service.getCelcius(kelvin);
+    }
 }
